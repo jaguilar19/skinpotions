a1cf0ae9105b2a0e062461fd961ce0eb328f67ff
juliusgtg <julius.aguilar6@gmail.com>
2016-07-04 21:12:19 +0800

    skinpotions

---
+++ resources/views/layout/header.blade.php
@@ -0,0 +1,9 @@
+<!-- Header -->
+    <header id="top" class="header img-responsive">
+        <div class="text-vertical-center">
+            <h1><img class="img-portfolio img-responsive" src="img/logo.png" /></h1>
+            <h3>Taglines or any message</h3>
+            <br>
+            <a href="#about" class="btn btn-dark btn-lg">Find Out More</a>
+        </div>
+    </header>
---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,71 @@
+@extends('layout.master')
+@section('title', 'Skin Potions')
+@section('content')
+<!-- About -->
+    <section id="about" class="about">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2>Put your Awesome lines here</h2>
+                    <p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu elementum velit, non interdum lacus. Vivamus volutpat, nisi ullamcorper facilisis varius, odio elit commodo mi, vitae efficitur ligula neque eu metus. Aenean nibh nunc, varius nec diam a, vulputate consequat mi. Morbi efficitur leo a convallis semper</a>.</p>
+                </div>
+            </div>
+            <!-- /.row -->
+        </div>
+        <!-- /.container -->
+    </section>
+
+    <!-- Products -->
+    <!-- The circle icons use Font Awesome's stacked icon classes. For more information, visit http://fontawesome.io/examples/ -->
+    <section id="services" class="services bg-primary">
+        <div class="container">
+            <div class="row text-center">
+                <div class="col-lg-10 col-lg-offset-1">
+                    <h2>Our Finest Products</h2>
+                    <hr class="small">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="portfolio-item">
+                                <a href="#">
+                                    <img class="img-portfolio img-responsive" src="img/portfolio-1.jpg">
+                                     <span class="btn btn-dark btn-lg">SHOP NOW!!</span>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="portfolio-item">
+                                <a href="#">
+                                    <img class="img-portfolio img-responsive" src="img/portfolio-2.jpg">
+                                     <span class="btn btn-dark btn-lg">SHOP NOW!!</span>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="portfolio-item">
+                                <a href="#">
+                                    <img class="img-portfolio img-responsive" src="img/portfolio-3.jpg">
+                                    <span class="btn btn-dark btn-lg">SHOP NOW!!</span>
+                                </a>       
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="portfolio-item">
+                                <a href="#">
+                                    <img class="img-portfolio img-responsive" src="img/portfolio-4.jpg">
+                                    <span class="btn btn-dark btn-lg">SHOP NOW!!</span>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- /.row (nested) -->
+                </div>
+                    </div>
+                    <!-- /.row (nested) -->
+                </div>
+                <!-- /.col-lg-10 -->
+            </div>
+            <!-- /.row -->
+        </div>
+        <!-- /.container -->
+    </section>
+@endsection
